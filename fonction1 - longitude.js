473f826dbafb1ae2f09ebaf89efb6b32924cefbd
xNexio <80476682+xNexio@users.noreply.github.com>
2021-03-23 11:46:00 +0100

    Add files via upload
    Première version des fonctions java qui récupèrent la longitude de l'utilisateur.

---
+++ fonction1 - longitude.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("message");
+var longitude_u = 0
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getLongitude);
+  } else { 
+    x.innerHTML = "La géolocation n'est pas compatible avec ce navigateur, cheh.";
+  }
+}
+
+function getLongitude(position) {
+  longitude_u = position.coords.longitude;
+}
+
