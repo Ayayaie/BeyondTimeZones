a7f2a207087b2f74ada4e992654b5e9d6a66deb4
Ayayaie <79145142+Ayayaie@users.noreply.github.com>
2021-03-24 20:38:23 +0100

    Add files via upload

---
+++ fonction3 - convertionHMS.js
@@ -0,0 +1,41 @@
+function conversionHMS_objects(temps) {
+	var reste = Math.abs(temps);
+    const resultat_objects = {signe:"+", heures:0, minutes:0, secondes:50};
+	var heures=Math.floor(reste/3600);
+	reste -= heures * 3600;
+	var minutes=Math.floor(reste/60);
+	reste -= minutes * 60;
+	var secondes=reste;
+    
+    if (temps < 0) {
+        resultat_objects.signe = "-";
+	} if (heures > 0) {
+        resultat_objects.heures = heures;
+	} if (minutes > 0) {
+        resultat_objects.minutes = minutes;
+	} if (secondes > 0) {
+        resultat_objects.secondes = secondes;
+    }
+	return resultat_objects;
+}
+
+function conversionHMS(temps) {
+	var reste = Math.abs(temps);
+	var resultat = '';
+	var heures=Math.floor(reste/3600);
+	reste -= heures * 3600;
+	var minutes=Math.floor(reste/60);
+	reste -= minutes * 60;
+	var secondes=reste;
+    
+    if (temps < 0) {
+    	resultat += "-";
+	} if (heures > 0) {
+		resultat += heures + 'h ';
+	} if (minutes > 0) {
+		resultat += minutes + 'min ';
+	} if (secondes > 0) {
+		resultat += secondes + 's ';
+    }
+	return resultat;
+}
