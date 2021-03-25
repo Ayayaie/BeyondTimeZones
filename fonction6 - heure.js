b0983203712eb19f2416b9e65a15b410197441ba
xNexio <80476682+xNexio@users.noreply.github.com>
2021-03-25 17:39:39 +0100

    Add files via upload
    Première version de la fonction qui rend l'heure locale

---
+++ fonction6 - heure.js
@@ -0,0 +1,20 @@
+function heure() {
+        date = new Date;
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        resultat = h+':'+m+':'+s;
+        return resultat;
+}
